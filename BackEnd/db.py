163ea4015f4f99a606afefd68a79c84df4d65fd0
Zihang-Xia <25659302+Zihang-Xia@users.noreply.github.com>
2021-04-07 16:46:23 +0800

    added db.py

---
+++ BackEnd/db.py
@@ -0,0 +1,22 @@
+import pymysql.cursors
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='password',
+                             database='flytrip_test',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+with connection:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import testData
+
 app = Flask(__name__)
 CORS(app, resources=r'/*', supports_credentials=True)
 
@@ -10,28 +11,30 @@ def POSTApi():
     req = request.json
     print('req:', req)
 
-    if req['action'] == 'register': # 注册请求
+    if req['action'] == 'register':  # 注册请求
         return jsonify({'status': 'failed',
                         'msg': 'Username is duplicated..'})
         # failed这里可以返回各种创建失败的原因，前端都会显示msg的内容
         return jsonify({'status': 'success'})
 
-    elif req['action'] == 'login': # 登录请求
+    elif req['action'] == 'login':  # 登录请求
         return jsonify({'status': 'failed',
                         'msg': 'Username and password are inconsistent.'})
         # failed这里可以返回各种创建失败的原因，前端都会显示msg的内容
         return jsonify({'status': 'success',
-                       'user_type': 'customer'})
-
+                        'user_type': 'customer'})
 
     return jsonify({'msg': 'Unknown action'})
+
 
 @app.route('/', methods=['GET'])
 def GETApi():
     print(request.args)
-    if request.args.get('action')=='getTickets': # 用户（非登录）查看所有票
+    if request.args.get('action') == 'getTickets':  # 用户（非登录）查看所有票
         return jsonify({'status': 'success',
                         'dataSource': testData.dataSource})
+
+
 # todo: session & token
 
 app.run(port=5000)
