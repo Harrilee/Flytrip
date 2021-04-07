ce5849246efeec7d04f6250abd3d0c8bf535d8f7
Zihang-Xia <25659302+Zihang-Xia@users.noreply.github.com>
2021-04-07 17:21:03 +0800

    added database initialization commands

--- BackEnd/__init__.py
+++ BackEnd/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -27,5 +28,3 @@ def create_app(test_config=None):
         return 'hello world!'
 
     return app
-
-
--- BackEnd/db.py
+++ BackEnd/db.py
@@ -1,11 +1,8 @@
 import pymysql.cursors
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='user',
-                             password='password',
-                             database='flytrip_test',
-                             cursorclass=pymysql.cursors.DictCursor)
 
 with connection:
     with connection.cursor() as cursor:
@@ -20,3 +17,43 @@ with connection:
         cursor.execute(sql, ('webmaster@python.org',))
         result = cursor.fetchone()
         print(result)
+
+
+def get_db():
+    if 'db' not in g:
+        g.db = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='password',
+            database='Air_Ticket',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.cursor().execute(f.read().decode('utf8'))
+
+
+@click.command('init_db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables"""
+    init_db()
+    click.echo('Database initialized.')
+
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
