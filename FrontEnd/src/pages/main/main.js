c4b3a0b41c8de6384eb445d2aed7d750d9de81a9
Harrilee <hl3794@nyu.edu>
2021-04-07 19:24:46 +0800

    Fixed combining problem. Now the project can run.

--- FrontEnd/src/pages/main/main.js
+++ FrontEnd/src/pages/main/main.js
@@ -3,14 +3,16 @@ import './main.css';
 
 import {Layout, Menu, Breadcrumb, Row, Col, Dropdown, Form, DatePicker, Input, Button, Mentions, Empty} from 'antd';
 
-import { UserOutlined, LaptopOutlined, NotificationOutlined,DownOutlined, SearchOutlined } from '@ant-design/icons';
+import {UserOutlined, LaptopOutlined, NotificationOutlined, DownOutlined, SearchOutlined} from '@ant-design/icons';
 
-const { Header, Content, Footer } = Layout;
+const {Header, Content, Footer} = Layout;
+
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
-function TableTitle(){
-    return(
+
+function TableTitle() {
+    return (
         <div style={{padding: '20px 0 0 0'}}>
             <div className={'ticket_title'}>
                 <Row gutter={16}>
@@ -31,25 +33,27 @@ function TableTitle(){
         </div>
     )
 }
-function Tickets(){
+
+function Tickets() {
     const [dataSource, setDataSource] = React.useState([]);
-    return(
+    return (
         <Content style={{padding: '50px 50px', minHeight: '90vh'}}>
             <div className="site-layout-content">
                 <div style={{margin: 'auto', maxWidth: '1000px'}}>
-                    <Form style={{padding: '20px', border: 'lightgrey solid 1px', borderRadius: '2px'}} onFinish={(form)=>{
-                        let url = 'http://localhost:5000?'
-                        url += 'action=getTickets'
-                        Object.keys(form).forEach(key=>{
-                            url += '&'+key+'='+form[key]
-                        })
-                        console.log(url)
-                        fetch(url)
-                            .then((resp) => resp.json())
-                            .then(data=>{
-                                setDataSource(data.dataSource)
-                            })
-                    }}>
+                    <Form style={{padding: '20px', border: 'lightgrey solid 1px', borderRadius: '2px'}}
+                          onFinish={(form) => {
+                              let url = 'http://localhost:5000?'
+                              url += 'action=getTickets'
+                              Object.keys(form).forEach(key => {
+                                  url += '&' + key + '=' + form[key]
+                              })
+                              console.log(url)
+                              fetch(url)
+                                  .then((resp) => resp.json())
+                                  .then(data => {
+                                      setDataSource(data.dataSource)
+                                  })
+                          }}>
                         <Row gutter={32}>
                             <Col span={6}>
                                 <Form.Item
@@ -83,141 +87,112 @@ function Tickets(){
                         </Row>
                     </Form>
                 </div>
-                {dataSource.length === 0? <Empty style={{margin:'100px 0'}}/> : <TableTitle />}
-                    {
-                        dataSource.map((d) => {
-                            return (
-                                <div className={'ticket'} key={d.key}>
-                                    <Row gutter={16} style={{minHeight: '100px'}} align={'middle'}
-                                         justify={'center'}>
-                                        <Col span={12}>
-                                            <div className={'airports'}>
-                                                {d.airline + ' | ' + d.flight_num}
-                                            </div>
-                                            <div className={'time'}>
-                                                <Row align={'space-around'}>
-                                                    <Col span={11} style={{textAlign: 'right'}}>
-                                                        {d.departure_time}
-                                                    </Col>
-                                                    <Col span={2}>
-
-                                                    </Col>
-                                                    <Col span={11} style={{textAlign: 'left'}}>
-                                                        {d.arrival_time}
-                                                    </Col>
-                                                </Row>
-                                            </div>
-                                            <div className={'airports'}>
-                                                <Row align={'middle'} justify={'bottomCenter'}>
-                                                    <Col span={11} style={{textAlign: 'right'}}>
-                                                        {d.depart_airport}
-                                                    </Col>
-                                                    <Col span={2}>
-                                                        <div style={{borderBottom:'black solid 0.5px', transform:'translateY(2px)', margin:'0 10px'}} />
-                                                    </Col>
-                                                    <Col span={11} style={{textAlign: 'left'}}>
-                                                        {d.arrive_airport}
-                                                    </Col>
-                                                </Row>
-                                            </div>
-                                            <div className={"duration"}>
-                                                {d.durationHour+'h '+d.durationMin+'min'}
-                                            </div>
-                                        </Col>
-                                        <Col span={4}>
-                                            <div className={'price'}>
-                                                {'￥' + formatNumber(d.ECprice)}
-                                            </div>
-                                        </Col>
-                                        <Col span={4}>
-                                            <div className={'price'}>
-                                                {'￥' + formatNumber(d.BCprice)}
-                                            </div>
-                                        </Col>
-                                        <Col span={4}>
-                                            <div className={'price'}>
-                                                {'￥' + formatNumber(d.FCprice)}
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                </div>
-                            )
-                        })
+                {()=> {
+                    if (dataSource === '') {
+                        return <React.Fragment/>
                     }
+                    else if (dataSource === []){
+                        return <Empty style={{margin: '100px 0'}}/>
+                }
+                    else{
+                        return <TableTitle/>
+                    }
+                }}
+                {
+                    dataSource.map((d) => {
+                        return (
+                            <div className={'ticket'} key={d.key}>
+                                <Row gutter={16} style={{minHeight: '100px'}} align={'middle'}
+                                     justify={'center'}>
+                                    <Col span={12}>
+                                        <div className={'airports'}>
+                                            {d.airline + ' | ' + d.flight_num}
+                                        </div>
+                                        <div className={'time'}>
+                                            <Row align={'space-around'}>
+                                                <Col span={11} style={{textAlign: 'right'}}>
+                                                    {d.departure_time}
+                                                </Col>
+                                                <Col span={2}>
+
+                                                </Col>
+                                                <Col span={11} style={{textAlign: 'left'}}>
+                                                    {d.arrival_time}
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                        <div className={'airports'}>
+                                            <Row align={'middle'} justify={'bottomCenter'}>
+                                                <Col span={11} style={{textAlign: 'right'}}>
+                                                    {d.depart_airport}
+                                                </Col>
+                                                <Col span={2}>
+                                                    <div style={{
+                                                        borderBottom: 'black solid 0.5px',
+                                                        transform: 'translateY(2px)',
+                                                        margin: '0 10px'
+                                                    }}/>
+                                                </Col>
+                                                <Col span={11} style={{textAlign: 'left'}}>
+                                                    {d.arrive_airport}
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                        <div className={"duration"}>
+                                            {d.durationHour + 'h ' + d.durationMin + 'min'}
+                                        </div>
+                                    </Col>
+                                    <Col span={4}>
+                                        <div className={'price'}>
+                                            {'￥' + formatNumber(d.ECprice)}
+                                        </div>
+                                    </Col>
+                                    <Col span={4}>
+                                        <div className={'price'}>
+                                            {'￥' + formatNumber(d.BCprice)}
+                                        </div>
+                                    </Col>
+                                    <Col span={4}>
+                                        <div className={'price'}>
+                                            {'￥' + formatNumber(d.FCprice)}
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </div>
+                        )
+                    })
+                }
             </div>
         </Content>
     )
 }
-function UpcomingFlights(){
-    const dataSource = [
-        {
-            key: '1',
-            airline: 'China Eastern Airline',
-            flight_num: 'MU5401',
-            departure_time: '08:10',
-            arrival_time: '11:05',
-            status:'upcoming',
-            arrive_city: 'Chengdu',
-            arrive_airport: 'Shuangliu International Airport',
-            depart_city: "Shanghai",
-            depart_airport: "Hongqiao International Airport",
-            durationHour: 3,
-            durationMin: 50
-        },
-        {
-            key: '2',
-            airline: 'China International Airline',
-            flight_num: 'CA9880',
-            departure_time: '09:10',
-            arrival_time: '12:05',
-            status:'upcoming',
-            arrive_city: 'Chengdu',
-            arrive_airport: 'Shuangliu International Airport',
-            depart_city: "Shanghai",
-            depart_airport: "Hongqiao International Airport",
-            durationHour: 2,
-            durationMin: 20
-        },
-        {
-            key: '2',
-            airline: 'China International Airline',
-            flight_num: 'CA9880',
-            departure_time: '09:10',
-            arrival_time: '12:05',
-            status:'delayed',
-            arrive_city: 'Chengdu',
-            arrive_airport: 'Shuangliu International Airport',
-            depart_city: "Shanghai",
-            depart_airport: "Hongqiao International Airport",
-            durationHour: 2,
-            durationMin: 20
-        },
-        {
-            key: '2',
-            airline: 'China International Airline',
-            flight_num: 'CA9880',
-            departure_time: '09:10',
-            arrival_time: '12:05',
-            status:'upcoming',
-            arrive_city: 'Chengdu',
-            arrive_airport: 'Shuangliu International Airport',
-            depart_city: "Shanghai",
-            depart_airport: "Hongqiao International Airport",
-            durationHour: 2,
-            durationMin: 20
-        },
-    ];
-    return(
+
+function UpcomingFlights() {
+    const [dataSource, setDataSource] = React.useState([])
+    return (
         <Content style={{padding: '50px 50px', minHeight: '90vh'}}>
             <div className="site-layout-content">
                 <div style={{margin: 'auto', maxWidth: '1000px'}}>
-                    <Form style={{padding: '20px', border: 'lightgrey solid 1px', borderRadius: '2px'}}>
+                    <Form style={{padding: '20px', border: 'lightgrey solid 1px', borderRadius: '2px'}}
+                          onFinish={(form) => {
+                              let url = 'http://localhost:5000?'
+                              url += 'action=getStatus'
+                              Object.keys(form).forEach(key => {
+                                  url += '&' + key + '=' + form[key]
+                              })
+                              console.log(url)
+                              fetch(url)
+                                  .then((resp) => resp.json())
+                                  .then(data => {
+                                      setDataSource(data.dataSource)
+                                  })
+                          }}>
                         <Row gutter={32} justify={'end'}>
                             <Col span={10}>
                                 <Form.Item
                                     name={'from'}
                                     label={"From"}>
-                                    <Input/>
+                                    <Input/> {/*todo 后面这些input全部改成mentions，随时返回提示*/}
                                 </Form.Item>
                             </Col>
                             <Col span={10}>
@@ -229,11 +204,10 @@ function UpcomingFlights(){
                             </Col>
                             <Col span={4}>
                                 <Button type={'primary'} onClick={() => {
-                                }} icon={<SearchOutlined/>}>
+                                }} icon={<SearchOutlined/>} htmlType={'submit'}>
                                     Search
                                 </Button>
                             </Col>
-
                         </Row>
                     </Form>
                 </div>
@@ -257,7 +231,7 @@ function UpcomingFlights(){
                     {
                         dataSource.map((d) => {
                             return (
-                                <div class={'ticket'}>
+                                <div className={'ticket'} key={d.key}>
                                     <Row gutter={16} style={{minHeight: '100px'}} align={'middle'}
                                          justify={'center'}>
                                         <Col span={12}>
@@ -270,7 +244,6 @@ function UpcomingFlights(){
                                                         {d.departure_time}
                                                     </Col>
                                                     <Col span={2}>
-
                                                     </Col>
                                                     <Col span={11} style={{textAlign: 'left'}}>
                                                         {d.arrival_time}
@@ -283,7 +256,11 @@ function UpcomingFlights(){
                                                         {d.depart_airport}
                                                     </Col>
                                                     <Col span={2}>
-                                                        <div style={{borderBottom:'black solid 0.5px', transform:'translateY(2px)', margin:'0 10px'}} />
+                                                        <div style={{
+                                                            borderBottom: 'black solid 0.5px',
+                                                            transform: 'translateY(2px)',
+                                                            margin: '0 10px'
+                                                        }}/>
                                                     </Col>
                                                     <Col span={11} style={{textAlign: 'left'}}>
                                                         {d.arrive_airport}
@@ -291,7 +268,7 @@ function UpcomingFlights(){
                                                 </Row>
                                             </div>
                                             <div className={"duration"}>
-                                                {d.durationHour+'h '+d.durationMin+'min'}
+                                                {d.durationHour + 'h ' + d.durationMin + 'min'}
                                             </div>
                                         </Col>
                                         <Col span={4}>
@@ -314,13 +291,13 @@ function UpcomingFlights(){
                             )
                         })
                     }
-
                 </div>
             </div>
         </Content>
     )
 
 }
+
 function Main(props) {
     const [mainMenu, setMainMenu] = React.useState('tickets')
 
@@ -330,7 +307,9 @@ function Main(props) {
                 <Header>
                     <Row justify={'space-between'}>
                         <Col>
-                            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[mainMenu]} onClick={e=>{setMainMenu(e.key)}}>
+                            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[mainMenu]} onClick={e => {
+                                setMainMenu(e.key)
+                            }}>
                                 <img src={'logo_white.png'} style={{display: 'inline'}} height={'50px'}/>
                                 <Menu.Item key="tickets">Tickets</Menu.Item>
                                 <Menu.Item key="upcoming_flights">Upcoming Flights</Menu.Item>
@@ -346,8 +325,8 @@ function Main(props) {
                         </Col>
                     </Row>
                 </Header>
-                {mainMenu=='tickets'?<Tickets />:<React.Fragment />}
-                {mainMenu=='upcoming_flights'?<UpcomingFlights />:<React.Fragment />}
+                {mainMenu == 'tickets' ? <Tickets/> : <React.Fragment/>}
+                {mainMenu == 'upcoming_flights' ? <UpcomingFlights/> : <React.Fragment/>}
                 <Footer style={{textAlign: 'center'}}>@Harry Lee, Zihang Xia | CSCI-SHU 213 Databases Course
                     Project</Footer>
             </Layout>
