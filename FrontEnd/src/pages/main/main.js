adda59da7292e17195eb7af2fd8d6ad681f8f9e0
Harrilee <hl3794@nyu.edu>
2021-05-01 00:09:47 +0800

    My fault, typo search -> search?

--- FrontEnd/src/pages/main/main.js
+++ FrontEnd/src/pages/main/main.js
@@ -62,7 +62,7 @@ function Tickets() {
                 <div style={{margin: 'auto', maxWidth: '1000px'}}>
                     <Form style={{padding: '20px', border: 'lightgrey solid 1px', borderRadius: '2px'}}
                           onFinish={(form) => {
-                              let url = 'http://localhost:5000/api/search'
+                              let url = 'http://localhost:5000/api/search?'
                               url += 'action=getTickets'
                               Object.keys(form).forEach(key => {
                                   url += '&' + key + '=' + form[key]
