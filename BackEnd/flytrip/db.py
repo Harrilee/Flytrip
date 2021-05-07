3a151e92463e2cedca209baed5117d8da505a1a0
Harrilee <hl3794@nyu.edu>
2021-05-07 21:22:04 +0800

    project setup update

--- BackEnd/flytrip/db.py
+++ BackEnd/flytrip/db.py
@@ -14,7 +14,7 @@ def get_db():
             host='localhost',
             port=3306,
             user='root',
-            password='password',
+
             database='flytrip',
             cursorclass=pymysql.cursors.DictCursor
         )
